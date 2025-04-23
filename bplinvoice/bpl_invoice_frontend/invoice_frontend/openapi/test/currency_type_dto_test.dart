775b7fed6f5e9078c11435527ff64f82533f8b23
Arjun7945 <arjunarjups111@gmail.com>
2025-04-23 14:36:30 +0530

    new server

---
+++ bplinvoice/bpl_invoice_frontend/invoice_frontend/openapi/test/currency_type_dto_test.dart
@@ -0,0 +1,21 @@
+import 'package:test/test.dart';
+import 'package:openapi/openapi.dart';
+
+// tests for CurrencyTypeDTO
+void main() {
+  final instance = CurrencyTypeDTOBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(CurrencyTypeDTO, () {
+    // int id
+    test('to test the property `id`', () async {
+      // TODO
+    });
+
+    // String name
+    test('to test the property `name`', () async {
+      // TODO
+    });
+
+  });
+}
